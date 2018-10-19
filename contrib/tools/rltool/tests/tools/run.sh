9d236c516a8ac7eef73951a0e79d8f406856d38b
Stephen Skeirik <skeirik2@illinois.edu>
2018-10-19 06:33:27 -0500

    rltool: improve test runner towards githook-ifying

--- contrib/tools/rltool/tests/tools/run.sh
+++ contrib/tools/rltool/tests/tools/run.sh
@@ -1,25 +1,33 @@
+#!/bin/bash
+
 DIR=$(dirname "$(readlink -f "$0")")
+
 pushd "$PWD"
+
 cd "$DIR"
-echo "ui-test1"
-maude -no-banner ui-test1.maude &> ui-test1.out
-diff ui-test1.out ui-test1.expected
-echo "ui-test2"
-maude -no-banner ui-test2.maude &> ui-test2.out
-diff ui-test2.out ui-test2.expected
-echo "proofrules"
-maude -no-banner proofrules.maude &> proofrules.out
-diff proofrules.out proofrules.expected
-echo "cstrterm"
-maude -no-banner cstrterm.maude &> cstrterm.out
-diff cstrterm.out cstrterm.expected
-echo "reachform"
-maude -no-banner reachform.maude &> reachform.out
-diff reachform.out reachform.expected
-echo "foform"
-maude -no-banner "foform.maude" &> "foform.out"
-diff "foform.out" "foform.expected"
-echo "impsimp"
-maude -no-banner impsimp.maude &> impsimp.out
-diff impsimp.out impsimp.expected
+
+run-test() {
+  maude -no-banner $1.maude &> $1.out
+  if [ $? -ne 0 ]; then
+     echo "maude died!"
+     return 1
+  fi
+  local OUT=$(diff "$1.expected" "$1.out" 2>&1)
+  if [ ! -z "$OUT" ]; then
+    echo "$1 failed"
+    return 1
+  fi 
+}
+
+FAIL=0
+
+for maudefile in $(ls *.maude); do
+  name="${maudefile%.*}"
+  echo "$name"
+  run-test "$name"
+  FAIL+=$?
+done
+
 popd
+
+exit $FAIL
